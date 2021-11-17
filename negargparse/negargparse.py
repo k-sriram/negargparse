f041ddc0fa3f1fd4c25958b4755593646c6fd4ef
Sriram Krishna <ksri91@gmail.com>
2021-11-18 01:12:16 +0530

    :hammer: Added automated pre-commit hooks
    - Some standard pre-commits
    - Format with black
    - Lint with flake8
    - Static type checking with mypy
    - Ensure that  __version__ is same in all modules
    - Code was modified so that it satisfies all tests

--- negargparse/negargparse.py
+++ negargparse/negargparse.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __all__ = [
     "sentinelise_args",
     "NegativeArgumentParser",
@@ -15,18 +15,8 @@ import re as _re
 import sys as _sys
 
 import argparse
+from typing import Sequence, Type
 
-from typing import TYPE_CHECKING as _TYPE_CHECKING
-
-if _TYPE_CHECKING:
-    from typing import (
-        Optional,
-        Sequence,
-        Type,
-        TypeVar,
-    )
-
-_T = TypeVar("_T")
 
 _negsentinel = "__minussign__"
 _negre = _re.compile(r"^-\d.*$")
@@ -34,7 +24,7 @@ _sentre = _re.compile(f"^{_negsentinel}")
 _minus = r"-"
 
 
-def sentinelise_args(args: list[str], sentinel: Optional[str] = _negsentinel) -> None:
+def sentinelise_args(args: list[str], sentinel: str = _negsentinel) -> None:
     for i, arg in enumerate(args):
         if _negre.fullmatch(arg) is not None:
             args[i] = f"{sentinel}{arg[1:]}"
@@ -43,8 +33,8 @@ def sentinelise_args(args: list[str], sentinel: Optional[str] = _negsentinel) ->
 class NegativeArgumentParser(argparse.ArgumentParser):
     def parse_known_args(
         self,
-        args: Optional[Sequence[str]] = None,
-        namespace: Optional[argparse.Namespace] = None,
+        args: Sequence[str] = None,
+        namespace: argparse.Namespace = None,
     ) -> tuple[argparse.Namespace, list[str]]:
         if args is None:
             # args default to the system args
@@ -56,27 +46,23 @@ class NegativeArgumentParser(argparse.ArgumentParser):
         return super().parse_known_args(args, namespace)
 
 
-# ignoring static type checking errors in the calling the __new__ functions
-# Known bug in mypy while calling static/class methods using super
-# See https://github.com/python/mypy/issues/9282
-
-# Also not explicitly checking for type of arg as it is only supposed
+# Not explicitly checking for type of arg as it is only supposed
 # to be called by argparse and it always provides a string.
 
 
 class NegInt(int):
     def __new__(cls: Type[NegInt], arg: str) -> NegInt:
         arg = _sentre.sub(_minus, arg, count=1)
-        return super().__new__(cls, arg)  # type: ignore[misc]
+        return super().__new__(cls, arg)
 
 
 class NegFloat(float):
     def __new__(cls: Type[NegFloat], arg: str) -> NegFloat:
         arg = _sentre.sub(_minus, arg, count=1)
-        return super().__new__(cls, arg)  # type: ignore[misc]
+        return super().__new__(cls, arg)
 
 
 class NegString(str):
     def __new__(cls: Type[NegString], arg: str) -> NegString:
         arg = _sentre.sub(_minus, arg, count=1)
-        return super().__new__(cls, arg)  # type: ignore[misc]
+        return super().__new__(cls, arg)
